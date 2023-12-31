5d21633ff5874a8f369167bd8eb42d0e70b68816
Pieter Provoost <pieterprovoost@gmail.com>
2023-12-31 12:09:27 +0100

    app icon

--- klara/klaraApp.swift
+++ klara/klaraApp.swift
@@ -1,17 +1,43 @@
-//
-//  klaraApp.swift
-//  klara
-//
-//  Created by Pieter Provoost on 30/12/2023.
-//
-
 import SwiftUI
+import AVKit
+
+struct AppMenu: View {
+
+    @State private var isPlaying = false
+    @State var player: AVPlayer?
+
+    let radioURL = URL(string: "http://icecast.vrtcdn.be/klara-high.mp3")!
+
+    func quitApp() {
+        NSApplication.shared.terminate(self)
+    }
+    
+    func togglePlay() {
+        isPlaying.toggle()
+
+        if self.player == nil {
+            let playerItem = AVPlayerItem(url: radioURL)
+            self.player = AVPlayer(playerItem: playerItem)
+        }
+
+        if isPlaying {
+            self.player?.play()
+        } else {
+            self.player?.pause()
+        }
+    }
+
+    var body: some View {
+        Button(action: togglePlay, label: { Text(isPlaying ? "Pause Klara" : "Play Klara") }).padding()
+        Button(action: quitApp, label: { Text("Quit") })
+    }
+}
 
 @main
 struct klaraApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("UtilityApp", systemImage: "music.note") {
+            AppMenu()
         }
     }
 }
