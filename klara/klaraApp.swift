15422e8e3f10f294784a69700051671c7b60db68
Pieter Provoost <pieterprovoost@gmail.com>
2024-01-01 00:58:49 +0100

    show metadata

--- klara/klaraApp.swift
+++ klara/klaraApp.swift
@@ -1,41 +1,64 @@
 import SwiftUI
 import AVKit
 
+class RadioStreamer: NSObject, ObservableObject {
+    @Published var itemTitle: String = "Klara"
+    
+    public let player: AVPlayer?
+    private let playerItem: AVPlayerItem?
+
+    override init() {
+        self.playerItem = AVPlayerItem(url: URL(string: "http://icecast.vrtcdn.be/klara-high.mp3")!)
+        self.player = AVPlayer(playerItem: self.playerItem)
+        super.init()
+
+        let metaOutput = AVPlayerItemMetadataOutput(identifiers: nil)
+        metaOutput.setDelegate(self, queue: DispatchQueue.main)
+        self.playerItem?.add(metaOutput)
+    }
+}
+
+extension RadioStreamer: AVPlayerItemMetadataOutputPushDelegate {
+    func metadataOutput(_ output: AVPlayerItemMetadataOutput, didOutputTimedMetadataGroups groups: [AVTimedMetadataGroup], from track: AVPlayerItemTrack?) {
+
+        for group in groups {
+            for item in group.items {
+                if let metadata = item as? AVMetadataItem, let metadataValue = metadata.value {
+                    self.itemTitle = metadataValue as! String
+                }
+            }
+        }
+    }
+}
+
 struct AppMenu: View {
-
     @Binding public var isPlaying: Bool
-    @State var player: AVPlayer?
-
-    let radioURL = URL(string: "http://icecast.vrtcdn.be/klara-high.mp3")!
+    @StateObject var streamer: RadioStreamer = RadioStreamer()
 
     func quitApp() {
         NSApplication.shared.terminate(self)
     }
-    
+
     func togglePlay() {
-        isPlaying.toggle()
-
-        if self.player == nil {
-            let playerItem = AVPlayerItem(url: radioURL)
-            self.player = AVPlayer(playerItem: playerItem)
-        }
-
         if isPlaying {
-            self.player?.play()
+            self.streamer.player?.pause()
         } else {
-            self.player?.pause()
+            self.streamer.player?.play()
         }
+        isPlaying.toggle()
     }
-
+    
     var body: some View {
-        Button(action: togglePlay, label: { Text(isPlaying ? "Pause Klara" : "Play Klara") }).padding()
-        Button(action: quitApp, label: { Text("Quit") })
+        VStack {
+            Text(self.streamer.itemTitle)
+            Button(action: togglePlay, label: { Text(isPlaying ? "Pause" : "Play") })
+            Button(action: quitApp, label: { Text("Quit") })
+        }
     }
 }
 
 @main
 struct klaraApp: App {
-
     @State public var isPlaying = false
 
     var body: some Scene {
